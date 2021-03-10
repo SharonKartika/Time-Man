771d16e7e9d09e3ecfd8796cc9d056fc00b9419b
Sharon Kartika <ksharon8256@gmail.com>
2021-03-10 14:08:17 +0530

    adding to firebase is done

--- readme.md
+++ readme.md
@@ -4,9 +4,9 @@ A tool used to choose the topic that you should study next and track the time yo
 https://sharonkartika.github.io/Time-Man/ 
 
 ### Features to be added
-- Option to add or remove tags to existsing topics. 
-- Let choose topic also specify the time for which the topic is to be studied. 
+- [ ] make `choose topic` functional
+- [ ] make `remove` functional
+- [ ] 
 
 
-- ~~add an `<hr>` tag to each entry in the tasklistcontainer.~~
-- ~~Add a "choose topic" button that chooses a topic~~
+
--- scripts.js
+++ scripts.js
@@ -2,37 +2,80 @@ let addTaskButton = document.getElementById('add-task-button');
 addTaskButton.addEventListener('click', getTopicDetails);
 topicListContainer = document.getElementById('topic-list-container');
 let chooseTopicButton = document.getElementById('choose-topic-button');
-chooseTopicButton.addEventListener('click',chooseTopic);
+chooseTopicButton.addEventListener('click', chooseTopic);
 let topicsList = [];
 
+// firebase config
+var firebaseConfig = {
+    apiKey: "AIzaSyACt7LWDcQ4LuMvuTNrh91_62pSbljaC_0",
+    authDomain: "time-man-c9f06.firebaseapp.com",
+    projectId: "time-man-c9f06",
+    storageBucket: "time-man-c9f06.appspot.com",
+    messagingSenderId: "637096490383",
+    appId: "1:637096490383:web:4e71cd01adceba1d4987fa"
+};
+
+firebase.initializeApp(firebaseConfig);
+var firestore = firebase.firestore();
+var docRef = firestore.doc("topics/topicList");
+
 displayToDOM();
-function Topic(title, desc, tags){
+function Topic(title, desc, tags) {
     this.title = title;
     this.desc = desc;
     this.tags = tags;
 }
 
-function getTopicDetails(){
+function getTopicDetails() {
     let title = prompt('Enter the title of the topic');
     let desc = prompt('Enter the description of the topic');
     let tags = prompt('Enter tags if any');
     let topic = new Topic(title, desc, tags);
-    localStorage.setItem(title,JSON.stringify(topic));
+    // localStorage.setItem(title,JSON.stringify(topic));
+    // store to firebase
+    addKeyValueToFireStore(topic);
     displayToDOM();
 }
 
-function displayToDOM(){
-    clearDOM();
-    for(let i=0;i<localStorage.length;i++){
-        let topic = JSON.parse(localStorage.getItem(localStorage.key(i)));
-        let topicNode = createNode(topic,i);
-    }
+function addKeyValueToFireStore(topic) {
+    //adds a key value pair to firestore
+    let title = topic.title;
+    let topicString = JSON.stringify(topic);
+    console.log(topicString);
+    //adds a key value pair
+    firestore.collection('topics').add({
+        title: topic.title,
+        desc: topic.desc,
+        tags: topic.tags
+
+    }).then(function () {
+        console.log('done!');
+    }).catch(function (error) {
+        console.log('got an error lmao', error);
+    });
 }
 
-function createNode(t,i){
+function displayToDOM() {
+    clearDOM();
+    firestore.collection('topics').get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            // console.log(`${doc.id} => ${doc.data().title}`);
+            let title = doc.data().title;
+            let desc = doc.data().desc;
+            let tags = doc.data().tags;
+            let topic = new Topic(title, desc, tags);             
+            createNode(topic,1);
+        })
+    });
+    // let topic = JSON.parse(localStorage.getItem(localStorage.key(i)));
+
+}
+
+
+function createNode(t, i) {
     //creates a node to be inserted from the topic object
     topicNode = document.createElement('div');
-    
+
     title = document.createElement('h3');
     title.textContent = t.title;
     topicNode.appendChild(title);
@@ -47,7 +90,7 @@ function createNode(t,i){
 
     removeBtn = document.createElement('button');
     removeBtn.textContent = 'REMOVE';
-    removeBtn.addEventListener('click',(e,i)=>{
+    removeBtn.addEventListener('click', (e, i) => {
         localStorage.removeItem(t.title);
         displayToDOM();
     })
@@ -60,21 +103,21 @@ function createNode(t,i){
 
 }
 
-function clearDOM(){
-    while(topicListContainer.firstElementChild){
+function clearDOM() {
+    while (topicListContainer.firstElementChild) {
         topicListContainer.removeChild(topicListContainer.firstElementChild);
     }
 }
 
-function chooseTopic(){
-    topicsList =[];
-    for(let i=0;i<localStorage.length; i++){
+function chooseTopic() {
+    topicsList = [];
+    for (let i = 0; i < localStorage.length; i++) {
         topicsList.push(localStorage.key(i));
     }
     let choice = getRandomIndexFromArray(topicsList);
     alert(topicsList[choice]);
 }
 
-function getRandomIndexFromArray(topicsList){
-    return Math.floor(Math.random()*topicsList.length)
+function getRandomIndexFromArray(topicsList) {
+    return Math.floor(Math.random() * topicsList.length)
 }
